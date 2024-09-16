26a80ac32873e8fe841ab5de7cb561890b96dede
deepsahuji <developerdeep1@gmail.com>
2024-09-16 23:45:27 +0530

    add

--- deepak-sahu/src/Component/LandingPage.jsx
+++ deepak-sahu/src/Component/LandingPage.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { ChakraProvider, Box, Flex, Heading, Spacer, Button } from '@chakra-ui/react';
 
 export default function LandingPage() {
   return (
     <ChakraProvider>
-      
+      <Flex
+        as="nav"
+        bg="teal.500"
+        color="white"
+        p={4}
+        align="center"
+        justify="space-between"
+      >
+        <Heading size="lg">MyApp</Heading>
+        <Spacer />
+        <Flex>
+          <Button variant="link" color="white" mr={4}>
+            Home
+          </Button>
+          <Button variant="link" color="white" mr={4}>
+            About
+          </Button>
+          <Button variant="link" color="white">
+            Contact
+          </Button>
+        </Flex>
+      </Flex>
+      {/* Add your page content here */}
     </ChakraProvider>
-  )
+  );
 }
